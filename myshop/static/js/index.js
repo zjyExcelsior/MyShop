4d8c48ffa4d7da7dc154e0f0ae3ce8c7f542593a
Zhu Jiongyao <zjywade@qq.com>
2016-05-17 23:46:11 +0800

    only for memory

---
+++ myshop/static/js/index.js
@@ -0,0 +1,11 @@
+var options = {
+    // autoplay: true,
+    speed: 1000, 
+    delay: 3000,
+    animation: 'vertical',
+    infinite: true,
+}
+
+$(function() {
+    $('.banner').unslider(options);
+});
