94ef342f00746dd02d9f5467dc09d12970806370
Zhu Jiongyao <zjywade@qq.com>
2016-06-09 00:11:54 +0800

    complete 100% functions

---
+++ myapp/static/js/goods.js
@@ -0,0 +1,37 @@
+//颜色按钮的点击事件
+$(".goods-list .color_icon").click(function(){
+    var img_url = $(this).attr("pic");
+    var color_id = $(this).attr("color_id");
+    var color_amount = $(this).attr("color_amount");
+    var img_div = $($(this).parents("li[product_id]").find(".image")[0]);
+    img_div.css("background-image", "url("+img_url+")");
+    img_div.attr("color_id", color_id);
+    img_div.attr("color_amount", color_amount);
+});
+
+
+
+//非详情商品添加购物车
+$(".rough_image").click(function(){
+    var color_amount = $(this).attr('color_amount');
+    var color_id = $(this).attr('color_id');
+    if(parseInt(color_amount) > 0){
+        $(this).parent().removeClass("toadd").toggleClass("carted");
+        $.ajax({
+            type: "POST", 
+            url: '/add_to_cart/',
+            data: JSON.stringify({ color_id: color_id}),
+            contentType: 'application/json',
+            success: function(data){
+                window.location.reload();
+            }
+        });
+    }
+    else{
+        var ab = $('<div class="alert-box">'+'该商品库存不足'+'</div>');
+        $("body").append(ab);
+        ab.show();
+        window.location.reload();
+    }
+});
+
