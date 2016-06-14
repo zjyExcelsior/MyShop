694fd170e95e79b0fa5a72e89b1ac1f8a9cb5af4
Zhu Jiongyao <zjywade@qq.com>
2016-06-15 03:47:51 +0800

    optimize the add to cart function

--- myapp/static/js/goods.js
+++ myapp/static/js/goods.js
@@ -12,11 +12,11 @@ $(".goods-list .color_icon").click(function(){
 
 
 //非详情商品添加购物车
-$(".rough_image").click(function(){
-    var color_amount = $(this).attr('color_amount');
-    var color_id = $(this).attr('color_id');
+$(".rough_image span").click(function(){
+    var color_amount = $(this).parent().attr('color_amount');
+    var color_id = $(this).parent().attr('color_id');
     if(parseInt(color_amount) > 0){
-        $(this).parent().removeClass("toadd").toggleClass("carted");
+        $(this).parent().parent().removeClass("toadd").toggleClass("carted");
         $.ajax({
             type: "POST", 
             url: '/add_to_cart/',
