314dab52f43f63f515b8dbf755b3a3c75219ba60
Zhu Jiongyao <zjywade@qq.com>
2016-06-17 23:45:12 +0800

    add Flask-Script

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from myapp import create_app, db
+from flask_script import Server, Manager
 
 def create_all_tables(app):
     with app.app_context():
@@ -13,4 +14,7 @@ if __name__ == '__main__':
     app = create_app('config')
     # drop_all_tables(app)
     create_all_tables(app)
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+    manager = Manager(app)
+    server = Server(host="0.0.0.0", port=5000, use_debugger=True, threaded=True)
+    manager.add_command("runserver", server)
+    manager.run()
