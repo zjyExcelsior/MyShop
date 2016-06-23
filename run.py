219feefb3d230992ed0d9208ef6020a0753e5b5f
Zhu Jiongyao <zjywade@qq.com>
2016-06-23 22:53:16 +0800

    optimize run.py

--- run.py
+++ run.py
@@ -2,19 +2,21 @@
 from myapp import create_app, db
 from flask_script import Server, Manager
 
-def create_all_tables(app):
-    with app.app_context():
-        db.create_all()
+app = create_app('config')
+manager = Manager(app)
+server = Server(host="0.0.0.0", port=5000, use_debugger=True, threaded=True)
+manager.add_command("runserver", server)
 
-def drop_all_tables(app):
-    with app.app_context():
-        db.drop_all()
+@manager.command
+def create_all_tables():
+    '''create all tables'''
+    db.create_all()
+
+
+@manager.command
+def drop_all_tables():
+    '''drop all tables'''
+    db.drop_all()
 
 if __name__ == '__main__':
-    app = create_app('config')
-    # drop_all_tables(app)
-    create_all_tables(app)
-    manager = Manager(app)
-    server = Server(host="0.0.0.0", port=5000, use_debugger=True, threaded=True)
-    manager.add_command("runserver", server)
     manager.run()
